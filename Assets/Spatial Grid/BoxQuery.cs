a478db8c439faa92b59a7fadb148a814fa9ec7bf
fedeventi <sangreguardia@gmail.com>
2023-07-26 15:17:47 -0300

    spatial grid

---
+++ Assets/Spatial Grid/BoxQuery.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxQuery : MonoBehaviour, IQuery //IA2-P2
+{
+    public SpatialGrid targetGrid;
+
+    public float width;
+    public float heigth;
+    
+
+    public IEnumerable<IGridEntity> selected = new List<IGridEntity>();
+
+    public void Start()
+    {
+        targetGrid = transform.parent.parent.parent.GetComponent<SpatialGrid>();
+    }
+
+    public IEnumerable<IGridEntity> Query()
+    {
+        var w = width ;
+        var h = heigth;
+        //var a = high * 0.5f;
+
+       return targetGrid.Query(
+                                transform.position + transform.forward + new Vector3(-w, 0, -h),
+                                transform.position + transform.forward + new Vector3(w, 0, h),                                
+                                x => true);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position + transform.forward , new Vector3(width, 1, heigth));
+       
+    }
+
+
+}
